da2233cc7ef678bb626a392a4ee74522fb16913a
SladognaB <bruno.sladogna@gmail.com>
2021-11-24 20:54:58 -0600

    esto subiendo mi primer repositorio

---
+++ html/clase1.js
@@ -0,0 +1,9 @@
+let numero1 = prompt("Ingresar el nùmero A");
+let numero2 = prompt("Ingresar numero b");
+
+if(numero1>numero2){
+    alert("El Numero A es mayor")
+}
+else{
+    alert("Nùmero B es mayor")
+}
